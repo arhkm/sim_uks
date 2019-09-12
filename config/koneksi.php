ca58608cbca66a27a60edca4c8b2a1d90a25e44f
Alfahasyim <m.alfahasyim@gmail.com>
2019-09-12 16:10:58 +0700

    fix-medis

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,5 +1,9 @@
-<?php  
+<?php
 
-	$con = mysqli_connect('localhost','root','','sim_uks');
+	$con = mysqli_connect('127.0.0.1','root','','sim_uks');
+
+	if (mysqli_connect_errno()){
+			echo "Koneksi database gagal : " . mysqli_connect_error();
+		}
 
 ?>
